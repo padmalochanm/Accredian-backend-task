30438ed491af9766b8b3d65b935ddefe31fdf4d4
Padmalochan <padmalochanmaiti33@gmail.com>
2024-07-06 00:09:24 +0530

    finally backend ready

---
+++ index.js
@@ -0,0 +1,164 @@
+import express from "express";
+import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import nodemailer from "nodemailer";
+import cors from "cors";
+
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+const prisma = new PrismaClient();
+const saltRounds = 10;
+const email = process.env.EMAIL;
+const password = process.env.PASSWORD;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: email, // Replace with your Gmail email
+    pass: password, // Replace with your Gmail password or App-specific password
+  },
+});
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.userId = user.userId;
+    next();
+  });
+};
+
+app.post("/register", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+
+    // Check if username or email already exists
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        OR: [{ username: username }, { email: email }],
+      },
+    });
+
+    if (existingUser) {
+      let message = "Username or email already in use";
+      if (existingUser.username === username) {
+        message = "Username already exists";
+      } else if (existingUser.email === email) {
+        message = "Email address already exists";
+      }
+      return res.status(409).json({ message });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    // Create new user using Prisma Client
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    const payload = { userId: newUser.id };
+    const secret = process.env.JWT_SECRET || "your_jwt_secret";
+    const token = jwt.sign(payload, secret, { expiresIn: "1hr" });
+    res.status(201).json({ message: "User registered successfully", token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+
+    // Find user by username
+    const user = await prisma.user.findUnique({
+      where: {
+        username: username,
+      },
+    });
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid username or password" });
+    }
+
+    // Compare hashed password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid username or password" });
+    }
+
+    // Generate JWT token
+    const payload = { userId: user.id };
+    const secret = process.env.JWT_SECRET || "your_jwt_secret";
+    const token = jwt.sign(payload, secret, { expiresIn: "1hr" });
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.post("/referral", authenticateToken, async (req, res) => {
+  try {
+    const { refereeName, refereeEmail, message } = req.body;
+    const referrerId = req.userId; // userId from authenticated token
+    if (!refereeName || !refereeEmail) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+
+    const newReferral = await prisma.referral.create({
+      data: {
+        referrerId,
+        refereeName,
+        refereeEmail,
+        message,
+      },
+    });
+
+    const mailOptions = {
+      from: "your-email@gmail.com",
+      to: refereeEmail,
+      subject: "You have been referred!",
+      text: `Hello ${refereeName},\n\nYou have been referred by someone at Accredian.\n\nMessage: ${message}`,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    res.status(201).json({
+      message: "Referral created successfully",
+      referral: newReferral,
+    });
+  } catch (error) {
+    console.error("Error creating referral:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running successfully on port ${PORT}`);
+});
---
+++ prisma/migrations/20240705135857_add_user_and_referral_tables/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `User_username_key`(`username`),
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Referral` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `referrerId` INTEGER NOT NULL,
+    `refereeName` VARCHAR(191) NOT NULL,
+    `refereeEmail` VARCHAR(191) NOT NULL,
+    `message` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Referral` ADD CONSTRAINT `Referral_referrerId_fkey` FOREIGN KEY (`referrerId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
